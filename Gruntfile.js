840940a9bf7789fd969ec4f79db47b188cfee213
Derek M. Anderson <derek.anderson@coxinc.com>
2014-05-15 21:53:15 -0500

    first pass

---
+++ Gruntfile.js
@@ -0,0 +1,38 @@
+/*
+ * grunt-enyo-builder
+ * https://github.com/derek/grunt-enyo-builder
+ *
+ * Copyright (c) 2014 Derek M. Anderson
+ * Licensed under the MIT license.
+ */
+
+'use strict';
+
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    // Configuration to be run (and then tested).
+    enyo_builder: {
+      default_options: {
+
+      },
+    },
+  });
+
+  // Actually load this plugin's task(s).
+  grunt.loadTasks('tasks');
+
+  // These plugins provide necessary tasks.
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-contrib-nodeunit');
+
+  // Whenever the "test" task is run, first clean the "tmp" dir, then run this
+  // plugin's task(s), then test the result.
+  grunt.registerTask('test', ['clean', 'enyo_builder', 'nodeunit']);
+
+  // By default, lint and run all tests.
+  grunt.registerTask('default', ['jshint', 'test']);
+
+};
---
+++ tasks/enyo_builder.js
@@ -0,0 +1,61 @@
+/*
+ * grunt-enyo-builder
+ * https://github.com/toxigenicpoem/grunt-enyo-builder
+ *
+ * Copyright (c) 2014 Derek M. Anderson
+ * Licensed under the MIT license.
+ */
+
+'use strict';
+
+module.exports = function (grunt) {
+
+    var path = require('path');
+    var fs = require('fs');
+    var modulepath = path.resolve('node_modules/grunt-enyo-builder');
+    var enyo = modulepath + '/enyo';
+
+    grunt.registerTask('enyo_builder', 'build an enyo component easily', function () {
+
+        var buildCmd = grunt.template.process('nodejs <%= deploypath %> -T -e <%= enyo %> -s <%= dir %> -o <%= dir %>/dist', {
+                data: {
+                    deploypath: enyo + '/tools/deploy.js',
+                    dir: process.cwd(),
+                    enyo: enyo,
+                    modulepath: modulepath
+                }
+            });
+
+        grunt.config.set('gitclone.enyoClone', {
+                options: {
+                    repository: 'https://github.com/enyojs/enyo.git',
+                    branch: 'master',
+                    directory: enyo
+                }
+        });
+
+        grunt.config.set('exec.enyoClone', {
+            cmd: buildCmd
+        });
+
+        grunt.config.set('clean.enyo', ['build']);
+
+        grunt.loadNpmTasks('grunt-git');
+        grunt.loadNpmTasks('grunt-contrib-clean');
+        grunt.loadNpmTasks('grunt-exec');
+
+        //grab enyo source for building
+        fs.exists(enyo, function(exists) {
+          if (!exists) {
+            grunt.task.run('gitclone:enyoClone');
+          }
+        });
+
+        grunt.task.run('exec:enyoClone');
+
+        grunt.task.run('clean');
+
+        grunt.verbose.ok();
+    });
+
+};
