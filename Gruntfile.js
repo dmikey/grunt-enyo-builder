a72afc2511fbfce8e1d695c633f6ebec8604a117
bart <bart@netage.nl>
2014-07-27 13:12:42 +0200

    Introduce subtasks for cloning and symlinking components

--- Gruntfile.js
+++ Gruntfile.js
@@ -10,29 +10,37 @@
 
 module.exports = function(grunt) {
 
-  // Project configuration.
-  grunt.initConfig({
-    // Configuration to be run (and then tested).
-    enyo_builder: {
-      default_options: {
+	// Project configuration.
+	grunt.initConfig({
+		// Configuration to be run (and then tested).
+		enyo_clone : {
+			default_options : {
 
-      },
-    },
-  });
+			},
+		},
+		enyo_builder : {
+			default_options : {
 
-  // Actually load this plugin's task(s).
-  grunt.loadTasks('tasks');
+			},
+		},
+	});
 
-  // These plugins provide necessary tasks.
-  grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.loadNpmTasks('grunt-contrib-clean');
-  grunt.loadNpmTasks('grunt-contrib-nodeunit');
+	// Actually load this plugin's task(s).
+	grunt.loadTasks('tasks');
 
-  // Whenever the "test" task is run, first clean the "tmp" dir, then run this
-  // plugin's task(s), then test the result.
-  grunt.registerTask('test', ['clean', 'enyo-builder', 'nodeunit']);
+	// These plugins provide necessary tasks.
+	grunt.loadNpmTasks('grunt-contrib-jshint');
+	grunt.loadNpmTasks('grunt-contrib-clean');
+	grunt.loadNpmTasks('grunt-contrib-nodeunit');
+	grunt.loadNpmTasks('grunt-contrib-symlink');
 
-  // By default, lint and run all tests.
-  grunt.registerTask('default', ['jshint', 'test']);
+	// Whenever the "test" task is run, first clean the "tmp" dir, then run this
+	// plugin's task(s), then test the result.
+	grunt.registerTask('test', [ 'clean', 'enyo-builder', 'nodeunit' ]);
+
+	// By default, lint and run all tests.
+	grunt.registerTask('build', [ 'jshint', 'test' ]);
+
+	grunt.registerTask('default', [ 'enyo-clone' ]);
 
 };
--- tasks/enyo_builder.js
+++ tasks/enyo_builder.js
@@ -8,62 +8,175 @@
 
 'use strict';
 
-module.exports = function (grunt) {
+module.exports = function(grunt) {
 
-    var path = require('path');
-    var fs = require('fs');
-    var modulepath = path.resolve('node_modules/grunt-enyo-builder');
-    var enyo = modulepath + '/enyo';
+	var path = require('path');
+	var fs = require('fs');
+	var modulepath = path.resolve('node_modules/grunt-enyo-builder');
+	var enyo = modulepath + '/enyo';
+	var layout = modulepath + '/layout';
+	var onyx = modulepath + '/onyx';
 
-    grunt.loadNpmTasks('grunt-git');
-    grunt.loadNpmTasks('grunt-contrib-clean');
-    grunt.loadNpmTasks('grunt-exec');
+	grunt.loadNpmTasks('grunt-git');
+	grunt.loadNpmTasks('grunt-contrib-clean');
+	grunt.loadNpmTasks('grunt-exec');
+	grunt.loadNpmTasks('grunt-contrib-symlink');
 
-    grunt.registerMultiTask('enyo-builder', 'build an enyo component easily', function () {
+	grunt
+			.registerMultiTask(
+					'enyo-builder',
+					'build an enyo component easily',
+					function() {
 
+						var options = this.options({
+							tag : '2.4.0',
+							lib : path.resolve('lib'),
+						});
 
-        var options = this.options({
-            tag: '2.4.0',
-            lib: path.resolve('lib'),
-        });
+						console.log(options);
+						var buildCmd = grunt.template
+								.process(
+										'nodejs <%= deploypath %> -T -e <%= enyo %> -s <%= dir %> -o <%= dir %>/dist -l <%= lib %>',
+										{
+											data : {
+												deploypath : enyo
+														+ '/tools/deploy.js',
+												dir : process.cwd(),
+												enyo : enyo,
+												modulepath : modulepath,
+												lib : options.lib
+											}
+										});
 
-        console.log(options);
+						grunt.config.set('exec.enyoDeploy', {
+							cmd : buildCmd
+						});
 
-        var buildCmd = grunt.template.process('nodejs <%= deploypath %> -T -e <%= enyo %> -s <%= dir %> -o <%= dir %>/dist -l <%= lib %>', {
-            data: {
-                deploypath: enyo + '/tools/deploy.js',
-                dir: process.cwd(),
-                enyo: enyo,
-                modulepath: modulepath,
-                lib: options.lib
-            }
-        });
+						grunt.config.set('clean.enyoBuild', [ 'build' ]);
+						grunt.task.run('exec:enyoDeploy');
+						grunt.task.run('clean:enyoBuild');
+						grunt.verbose.ok();
 
-        grunt.config.set('gitclone.enyoClone', {
-            options: {
-                repository: 'https://github.com/enyojs/enyo.git',
-                branch: options.tag,
-                directory: enyo
-            }
-        });
+					});
 
-        grunt.config.set('exec.enyoClone', {
-            cmd: buildCmd
-        });
+	grunt.registerTask('enyo-clone', 'clone enyo', function() {
 
-        grunt.config.set('clean.enyoBuild', ['build']);
+		var options = this.options({
+			tag : '2.4.0',
+			lib : path.resolve('lib'),
+		});
 
-        try {
-            // Query the entry
-            var files = fs.readdirSync(enyo);
-        } catch (e) {
-            grunt.task.run('gitclone:enyoClone');
-        }
+		console.log(options);
 
-        grunt.task.run('exec:enyoClone');
-        grunt.task.run('clean:enyoBuild');
-        grunt.verbose.ok();
+		grunt.config.set('gitclone.enyoClone', {
+			options : {
+				repository : 'https://github.com/enyojs/enyo.git',
+				branch : options.tag,
+				directory : enyo
+			}
+		});
 
-    });
+		grunt.config.set('symlink.enyoLink', {
+			// Enable overwrite to delete symlinks before recreating them
+			options : {
+				overwrite : false
+			},
+			files : {
+				'enyo' : 'node_modules/grunt-enyo-builder/enyo',
+			}
+		});
+		
+		try {
+			// Query the entry
+			var files = fs.readdirSync(enyo);
+		} catch (e) {
+			grunt.task.run('gitclone:enyoClone');
+			
+		}
+		grunt.task.run('symlink:enyoLink');
+		
+		grunt.verbose.ok();
+	});
+	
+	grunt.registerTask('layout-clone', 'clone layout', function() {
 
+		var options = this.options({
+			tag : '2.4.0',
+			lib : path.resolve('lib'),
+		});
+
+		console.log(options);
+
+		grunt.config.set('gitclone.layoutClone', {
+			options : {
+				repository : 'https://github.com/enyojs/layout.git',
+				branch : options.tag,
+				directory : layout
+			}
+		});
+
+		grunt.config.set('symlink.layoutLink', {
+			// Enable overwrite to delete symlinks before recreating them
+			options : {
+				overwrite : false
+			},
+			files : {
+				'lib/contrib/layout': layout
+				
+			}
+		});
+		
+		try {
+			// Query the entry
+			var files = fs.readdirSync(layout);
+		} catch (e) {
+			grunt.task.run('gitclone:layoutClone');
+			
+		}
+		console.log('gitclone.layoutClone');
+		grunt.task.run('symlink:layoutLink');
+		
+		grunt.verbose.ok();
+	});
+	
+	grunt.registerTask('onyx-clone', 'clone onyx', function() {
+
+		var options = this.options({
+			tag : '2.4.0',
+			lib : path.resolve('lib'),
+		});
+
+		console.log(options);
+
+		grunt.config.set('gitclone.onyxClone', {
+			options : {
+				repository : 'https://github.com/enyojs/onyx.git',
+				branch : options.tag,
+				directory : onyx
+			}
+		});
+
+		grunt.config.set('symlink.onyxLink', {
+			// Enable overwrite to delete symlinks before recreating them
+			options : {
+				overwrite : false
+			},
+			files : {
+				'lib/contrib/onyx': onyx
+				
+			}
+		});
+		
+		try {
+			// Query the entry
+			var files = fs.readdirSync(onyx);
+		} catch (e) {
+			grunt.task.run('gitclone:onyxClone');
+			
+		}
+		console.log('gitclone.onyxClone');
+		grunt.task.run('symlink:onyxLink');
+		
+		grunt.verbose.ok();
+	});
 };
